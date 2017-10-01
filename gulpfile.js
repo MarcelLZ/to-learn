289dea4c2a5a58f9d94ebf037ca0c9bf64431ce6
vmarcosp <contato@vmarcosp.com.br>
2017-10-01 20:46:28 -0300

    Creatad task for development

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,23 @@
 const gulp = require('gulp');
+const gulpSass = require('gulp-sass');
+const browserSync = require('browser-sync');
 
-gulp.task('dev', ()=> console.log('Task for development...'));
-gulp.task('build', ()=> console.log('Task for development...'));
+gulp.task('dev', ['scss'], function () {
+
+	browserSync.init({
+		server: './docs',
+		open: false
+	});
+
+	gulp.watch('./**/*.scss', ['scss']);
+	gulp.watch('docs/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('scss', function () {
+	return gulp.src('to-learn.scss')
+		.pipe(gulpSass())
+		.pipe(gulp.dest('docs/css'))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('build', () => console.log('Task for development...'));
